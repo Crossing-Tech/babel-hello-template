47ebbb306e1ef01c54d674e2e5fcc8119f17dde7
Christophe Pache <christophe.pache@crossing-tech.com>
2014-10-27 14:45:31 +0100

    improved tutorial

--- src/main/scala/HelloRoute.scala
+++ src/main/scala/HelloRoute.scala
@@ -1,9 +1,28 @@
 import io.xtech.babel.camel.builder.RouteBuilder
-import io.xtech.babel.camel.model.Aggregation.{CompletionSize, FoldBody}
-import io.xtech.babel.fish.model.Message
 import io.xtech.babel.camel.mock._
+import org.apache.camel.impl.DefaultCamelContext
+
+import scala.collection.immutable
+
 
 class HelloRoute extends RouteBuilder with Mock {
 
-    from("direct:hello").processBody(x => "Hello " + x).log("${body}")
+  from("direct:hello").log("Hello ${body}")
 }
+
+object HelloRoute {
+  private val vowel = immutable.Set('a', 'e', 'i', 'o', 'u', 'y')
+  
+  def vowelAndConsonant(str: String): (Array[Char],Array[Char]) = str.toCharArray.partition(vowel.contains)
+}
+
+object Main extends App {
+  val context = new DefaultCamelContext
+  new HelloRoute().addRoutesToCamelContext(context)
+  context.start
+
+  //sends "World" to the route
+  context.createProducerTemplate().sendBody("direct:hello", "World")
+
+}
+
